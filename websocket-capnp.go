9ebea22406ad150dd15019a03067bcf81188fc1f
Ian Denhardt <ian@zenhack.net>
2022-11-20 16:13:54 -0500

    Remove xz compression and unnecessary Port abstraction.
    Web sockets have a built-in compression mechanism anyway.

--- websocket-capnp.go
+++ websocket-capnp.go
@@ -7,80 +7,16 @@ import (
 	"time"
 
 	"capnproto.org/go/capnp/v3"
-	"capnproto.org/go/capnp/v3/rpc"
 	"capnproto.org/go/capnp/v3/rpc/transport"
 	"github.com/gorilla/websocket"
-	"github.com/ulikunitz/xz"
 )
 
-type Port interface {
-	NextWriter(context.Context) (io.WriteCloser, error)
-	NextReader(context.Context) (io.Reader, error)
-	Close() error
-}
-
-func WebSocketPort(conn *websocket.Conn) Port {
-	return websocketPort{conn}
-}
-
-func XZPort(p Port) Port {
-	return transformPort{
-		underlying: p,
-		wrapR: func(r io.Reader) (io.Reader, error) {
-			return xz.NewReader(r)
-		},
-		wrapW: func(w io.WriteCloser) (io.WriteCloser, error) {
-			return xz.NewWriter(w)
-		},
-	}
-}
-
-func PortCodec(p Port) transport.Codec {
-	return portCodec{p}
-}
-
-type websocketPort struct {
+type websocketCodec struct {
 	conn *websocket.Conn
 }
 
-func (p websocketPort) NextWriter(ctx context.Context) (io.WriteCloser, error) {
-	if err := ctx.Err(); err != nil {
-		return nil, err
-	}
-	return p.conn.NextWriter(websocket.BinaryMessage)
-}
-
-func (p websocketPort) NextReader(ctx context.Context) (io.Reader, error) {
-	var (
-		typ int
-		r   io.Reader
-		err error
-	)
-	for ctx.Err() == nil && typ != websocket.BinaryMessage {
-		typ, r, err = p.conn.NextReader()
-		if err != nil {
-			return nil, err
-		}
-		if typ == websocket.PingMessage {
-			err = p.conn.WriteMessage(websocket.PongMessage, nil)
-			if err != nil {
-				return nil, err
-			}
-		}
-	}
-	return r, ctx.Err()
-}
-
-func (p websocketPort) Close() error {
-	return p.conn.Close()
-}
-
-type portCodec struct {
-	p Port
-}
-
-func (c portCodec) Encode(ctx context.Context, msg *capnp.Message) error {
-	w, err := c.p.NextWriter(ctx)
+func (c websocketCodec) Encode(ctx context.Context, msg *capnp.Message) error {
+	w, err := c.conn.NextWriter(websocket.BinaryMessage)
 	if err != nil {
 		return err
 	}
@@ -88,48 +24,40 @@ func (c portCodec) Encode(ctx context.Context, msg *capnp.Message) error {
 	return capnp.NewEncoder(w).Encode(msg)
 }
 
-func (c portCodec) Decode(ctx context.Context) (*capnp.Message, error) {
-	r, err := c.p.NextReader(ctx)
-	if err != nil {
-		return nil, err
+func (c websocketCodec) Decode(ctx context.Context) (*capnp.Message, error) {
+	var (
+		typ int
+		r   io.Reader
+		err error
+	)
+	for ctx.Err() == nil && typ != websocket.BinaryMessage {
+		typ, r, err = c.conn.NextReader()
+		if err != nil {
+			return nil, err
+		}
+		if typ == websocket.PingMessage {
+			err = c.conn.WriteMessage(websocket.PongMessage, nil)
+			if err != nil {
+				return nil, err
+			}
+		}
 	}
 	return capnp.NewDecoder(r).Decode()
 }
 
-func (c portCodec) Close() error {
-	return c.p.Close()
+func (p websocketCodec) Close() error {
+	return p.conn.Close()
 }
 
-func (portCodec) SetPartialWriteTimeout(time.Duration) {}
+func (websocketCodec) SetPartialWriteTimeout(time.Duration) {}
 
-type transformPort struct {
-	underlying Port
-	wrapR      func(io.Reader) (io.Reader, error)
-	wrapW      func(io.WriteCloser) (io.WriteCloser, error)
+// Return a transport.Codec that sends messages over the websocket connection.
+// Sends each capnproto message in its own websocket binary message.
+func NewCodec(conn *websocket.Conn) transport.Codec {
+	return websocketCodec{conn}
 }
 
-func (p transformPort) NextWriter(ctx context.Context) (io.WriteCloser, error) {
-	w, err := p.underlying.NextWriter(ctx)
-	if err != nil {
-		return nil, err
-	}
-	return p.wrapW(w)
-}
-
-func (p transformPort) NextReader(ctx context.Context) (io.Reader, error) {
-	r, err := p.underlying.NextReader(ctx)
-	if err != nil {
-		return nil, err
-	}
-	return p.wrapR(r)
-}
-
-func (p transformPort) Close() error {
-	return p.underlying.Close()
-}
-
-// Return an rpc.Transport that sends messages over the websocket connection.
-// Sends each capnproto message in its own websocket binary message, xz-compressed.
-func NewTransport(conn *websocket.Conn) rpc.Transport {
-	return transport.New(PortCodec(XZPort(WebSocketPort(conn))))
+// Like NewCodec, but returns a transport.Transport.
+func NewTransport(conn *websocket.Conn) transport.Transport {
+	return transport.New(NewCodec(conn))
 }
